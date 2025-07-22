5255d9c3878e779a12c70fe14f1998f8d92d8f50
pasha <askerovpasha21@gmail.com>
2025-07-22 19:02:48 +0400

    complete about section

--- js/script.js
+++ js/script.js
@@ -2,6 +2,13 @@ document.addEventListener('DOMContentLoaded', function () {
     $(document).ready(function () {
         $('.menu-toggle-button').click(function () {
             $(this).toggleClass('active');
-        })
+        });
+
+        $(window).on('scroll', function () {
+            let scrollTop = $(window).scrollTop();
+            let rotateDeg = (scrollTop * 0.03) % 360;
+
+            $('.about-shape img').css('transform', 'rotate(' + rotateDeg + 'deg)');
+        });
     });
 });
